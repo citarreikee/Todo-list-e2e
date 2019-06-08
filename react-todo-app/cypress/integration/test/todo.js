6c15d9d06eb30889424e69a4fd1c6359b7c19274
citarreikee <citarreikee@gmail.com>
2019-06-08 13:56:22 +0800

    Update cases

--- react-todo-app/cypress/integration/test/todo.js
+++ react-todo-app/cypress/integration/test/todo.js
@@ -2,21 +2,21 @@ describe("todolist test",() => {
     it("add a new todo task", () => {
         cy.visit("http://localhost:8080");
         cy.wait(1000);
-        cy.get('#app > div > form > input').type('吃海底捞');
+        cy.get('#app > div > form > input').type('班级聚餐');
         cy.get('#app > div > form > button').click();
         cy.wait(1000);
 
 
     });
     it("add a new todo task", () => {
-        cy.get('#app > div > form > input').type('完成web作业');
+        cy.get('#app > div > form > input').type('课题组聚餐');
         cy.get('#app > div > form > button').click();
         cy.wait(1000);
     });
     it("edit first todo task", () => {
         cy.get('#app > div > table > tbody > tr:nth-child(1) > td:nth-child(2) > button:nth-child(1)').click()
         cy.get('#app > div > table > tbody > tr:nth-child(1) > td:nth-child(1) > form > input').clear();
-        cy.get('#app > div > table > tbody > tr:nth-child(1) > td:nth-child(1) > form > input').type('吃小龙坎');
+        cy.get('#app > div > table > tbody > tr:nth-child(1) > td:nth-child(1) > form > input').type('生日聚餐');
         cy.wait(1000);
         cy.get("#app > div > table > tbody > tr:nth-child(1) > td:nth-child(2) > button:nth-child(1)").click()
         cy.wait(1000);
