f606ec7a2c2847bc47a34f9dc6827a002db04ed8
citarreikee <citarreikee@gmail.com>
2019-06-08 15:31:51 +0800

    Upgrade backstop

--- react-todo-app/backstop_data/html_report/config.js
+++ react-todo-app/backstop_data/html_report/config.js
@@ -3,111 +3,52 @@ report({
   "tests": [
     {
       "pair": {
-        "reference": "..\\bitmaps_reference\\backstop_default_todo_app_homepage_0__0_phone.png",
-        "test": "..\\bitmaps_test\\20190607-115702\\backstop_default_todo_app_homepage_0__0_phone.png",
-        "selector": "",
-        "fileName": "backstop_default_todo_app_homepage_0__0_phone.png",
-        "label": "todo app homepage",
+        "reference": "..\\bitmaps_reference\\backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
+        "test": "..\\bitmaps_test\\20190608-152904\\backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
+        "label": "BackstopJS Homepage",
         "requireSameDimensions": true,
-        "misMatchThreshold": 10,
+        "misMatchThreshold": 5,
         "url": "http://localhost:8080/",
         "referenceUrl": "",
         "expect": 0,
         "viewportLabel": "phone",
-        "engineErrorMsg": "require(...) is not a function",
-        "error": "Reference file not found C:\\Users\\mayn\\Desktop\\react-todo-app\\backstop_data\\bitmaps_reference\\backstop_default_todo_app_homepage_0__0_phone.png"
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
       },
-      "status": "fail"
+      "status": "pass"
     },
     {
       "pair": {
-        "reference": "..\\bitmaps_reference\\backstop_default_todo_app_homepage_0__1_tablet.png",
-        "test": "..\\bitmaps_test\\20190607-115702\\backstop_default_todo_app_homepage_0__1_tablet.png",
-        "selector": "",
-        "fileName": "backstop_default_todo_app_homepage_0__1_tablet.png",
-        "label": "todo app homepage",
+        "reference": "..\\bitmaps_reference\\backstop_default_BackstopJS_Homepage_0_document_1_tablet.png",
+        "test": "..\\bitmaps_test\\20190608-152904\\backstop_default_BackstopJS_Homepage_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_default_BackstopJS_Homepage_0_document_1_tablet.png",
+        "label": "BackstopJS Homepage",
         "requireSameDimensions": true,
-        "misMatchThreshold": 10,
+        "misMatchThreshold": 5,
         "url": "http://localhost:8080/",
         "referenceUrl": "",
         "expect": 0,
         "viewportLabel": "tablet",
-        "engineErrorMsg": "require(...) is not a function",
-        "error": "Reference file not found C:\\Users\\mayn\\Desktop\\react-todo-app\\backstop_data\\bitmaps_reference\\backstop_default_todo_app_homepage_0__1_tablet.png"
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "2.16",
+          "analysisTime": 17
+        }
       },
-      "status": "fail"
-    },
-    {
-      "pair": {
-        "reference": "..\\bitmaps_reference\\backstop_default_todo_app_homepage_0__2_desktop.png",
-        "test": "..\\bitmaps_test\\20190607-115702\\backstop_default_todo_app_homepage_0__2_desktop.png",
-        "selector": "",
-        "fileName": "backstop_default_todo_app_homepage_0__2_desktop.png",
-        "label": "todo app homepage",
-        "requireSameDimensions": true,
-        "misMatchThreshold": 10,
-        "url": "http://localhost:8080/",
-        "referenceUrl": "",
-        "expect": 0,
-        "viewportLabel": "desktop",
-        "engineErrorMsg": "require(...) is not a function",
-        "error": "Reference file not found C:\\Users\\mayn\\Desktop\\react-todo-app\\backstop_data\\bitmaps_reference\\backstop_default_todo_app_homepage_0__2_desktop.png"
-      },
-      "status": "fail"
-    },
-    {
-      "pair": {
-        "reference": "..\\bitmaps_reference\\backstop_default_add_new_task_0__0_phone.png",
-        "test": "..\\bitmaps_test\\20190607-115702\\backstop_default_add_new_task_0__0_phone.png",
-        "selector": "",
-        "fileName": "backstop_default_add_new_task_0__0_phone.png",
-        "label": "add new task",
-        "requireSameDimensions": true,
-        "misMatchThreshold": 10,
-        "url": "http://localhost:8080/",
-        "referenceUrl": "",
-        "expect": 0,
-        "viewportLabel": "phone",
-        "engineErrorMsg": "require(...) is not a function",
-        "error": "Reference file not found C:\\Users\\mayn\\Desktop\\react-todo-app\\backstop_data\\bitmaps_reference\\backstop_default_add_new_task_0__0_phone.png"
-      },
-      "status": "fail"
-    },
-    {
-      "pair": {
-        "reference": "..\\bitmaps_reference\\backstop_default_add_new_task_0__1_tablet.png",
-        "test": "..\\bitmaps_test\\20190607-115702\\backstop_default_add_new_task_0__1_tablet.png",
-        "selector": "",
-        "fileName": "backstop_default_add_new_task_0__1_tablet.png",
-        "label": "add new task",
-        "requireSameDimensions": true,
-        "misMatchThreshold": 10,
-        "url": "http://localhost:8080/",
-        "referenceUrl": "",
-        "expect": 0,
-        "viewportLabel": "tablet",
-        "engineErrorMsg": "require(...) is not a function",
-        "error": "Reference file not found C:\\Users\\mayn\\Desktop\\react-todo-app\\backstop_data\\bitmaps_reference\\backstop_default_add_new_task_0__1_tablet.png"
-      },
-      "status": "fail"
-    },
-    {
-      "pair": {
-        "reference": "..\\bitmaps_reference\\backstop_default_add_new_task_0__2_desktop.png",
-        "test": "..\\bitmaps_test\\20190607-115702\\backstop_default_add_new_task_0__2_desktop.png",
-        "selector": "",
-        "fileName": "backstop_default_add_new_task_0__2_desktop.png",
-        "label": "add new task",
-        "requireSameDimensions": true,
-        "misMatchThreshold": 10,
-        "url": "http://localhost:8080/",
-        "referenceUrl": "",
-        "expect": 0,
-        "viewportLabel": "desktop",
-        "engineErrorMsg": "require(...) is not a function",
-        "error": "Reference file not found C:\\Users\\mayn\\Desktop\\react-todo-app\\backstop_data\\bitmaps_reference\\backstop_default_add_new_task_0__2_desktop.png"
-      },
-      "status": "fail"
+      "status": "pass"
     }
   ],
   "id": "backstop_default"
